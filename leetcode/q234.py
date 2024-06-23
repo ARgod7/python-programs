c98278b1ace70d8cee421817f1ace95d5c76c2bc
ARgod7 <krishanarpit2003@gmail.com>
2024-06-23 13:23:53 +0530

    234

---
+++ leetcode/q234.py
@@ -0,0 +1,20 @@
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def isPalindrome(head: Optional[ListNode]) -> bool:
+        arr = []
+        while head:
+            arr.append(head.val)
+            head = head.next
+        l , r = 0 , len(arr)-1
+        while l <= r:
+            if arr[l] != arr[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+isPalindrome([1,2,2,1])
+        
