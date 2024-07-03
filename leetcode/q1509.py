3678288c91b3ba95ee0b761e10f83ee80a28ed78
ARgod7 <krishanarpit2003@gmail.com>
2024-07-03 10:48:56 +0530

    1509

---
+++ leetcode/q1509.py
@@ -0,0 +1,11 @@
+import heapq
+def minDifference(self, nums: list[int]) -> int:
+        if len(nums) <= 4:
+            return 0
+        min_four = sorted(heapq.nsmallest(4,nums))
+        max_four = sorted(heapq.nlargest(4,nums))
+        res = float("inf")
+
+        for i in range(4):
+            res = min(res,max_four[i]-min_four[i])
+        return res
