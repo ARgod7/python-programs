c3c16843bbcfade40dbbf091fef1a28a597ec762
ARgod7 <krishanarpit2003@gmail.com>
2024-07-14 15:09:32 +0530

    338

---
+++ leetcode/q338.py
@@ -0,0 +1,14 @@
+def countBits(n: int) -> list[int]:
+        # dp = [0] * (n+1)
+        # offset = 1
+        # for i in range(1, n + 1):
+        #     if offset * 2 == i:
+        #         offset = i
+        #     dp[i] = 1 + dp[i - offset]
+        # return dp
+        counter = [0]
+        for i in range(1, n+1):
+            counter.append(counter[i // 2] + i % 2)
+        return counter
+countBits(17)
+        
